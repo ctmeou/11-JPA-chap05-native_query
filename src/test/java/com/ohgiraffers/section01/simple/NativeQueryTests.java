101a67e2ef8c471dbca56493fa3cf26c7ff48d58
ctmeou <ictmeousz@gmail.com>
2023-10-26 11:47:14 +0900

    10/26 himedia 11.JPA - 05.native-query

---
+++ src/test/java/com/ohgiraffers/section01/simple/NativeQueryTests.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.section01.simple;
+
+import org.junit.jupiter.api.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class NativeQueryTests {
+
+    //application 당 1개만
+    private static EntityManagerFactory entityManagerFactory;
+
+    //스레드 세이프 하지 않고, 요청 당 1개
+    private EntityManager entityManager;
+
+    @BeforeAll //junit에서 오는 어노테이션, 테스트가 진행되기 전에 한 번 진행된다.
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach //테스트 하나가 진행되기 전에 한 번씩
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll //테스트가 끝나기 전에 한 번만
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach //테스트가 끝날 떄마다 한 번씩
+    public void closeManager() {
+        entityManager.close();
+    }
+
+//    @Test
+//    public void
+
+
+
+}
